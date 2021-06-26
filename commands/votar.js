392f6a53764ab1a9b40775efdc8ff34b5c74478c
Humberto <lukas.95_@hotmail.com>
2021-06-26 05:36:03 -0700

    Adicionado comando votar

---
+++ commands/votar.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  info: {
+    name: "votar",
+    description: "Para votar no bot",
+    usage: "",
+    aliases: ["vote", " vt"],
+  },
+
+  run: async function (client, message, args) {
+
+    let avatar = message.author.displayAvatarURL({format: 'png'});
+
+    const votar = new MessageEmbed()
+    .setAuthor("Nagatoro Music")
+    .setThumbnail(avatar)
+    .setDescription(`**Ajude a Nagatoro Music votando nele!**\n\n*Para votar clique nos links abaixo:*\n\n **Nagoto Music (top.gg)**: [clique aqui](https://top.gg/bot/842498634822189096/vote)`)
+    .setColor("#22a7cc")
+    .setFooter("© Nagatoro Music - https://nagatoro.waaclive.com")
+message.channel.send(message.author, votar)
+
+  },
+};
