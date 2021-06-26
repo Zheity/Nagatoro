1029b6e058ebf76e562af94bf24d70d1ef74c170
Humberto <lukas.95_@hotmail.com>
2021-06-26 05:37:17 -0700

    update comando votar

--- commands/votar.js
+++ commands/votar.js
@@ -15,7 +15,7 @@ module.exports = {
     const votar = new MessageEmbed()
     .setAuthor("Nagatoro Music")
     .setThumbnail(avatar)
-    .setDescription(`**Ajude a Nagatoro Music votando nele!**\n\n*Para votar clique nos links abaixo:*\n\n **Nagoto Music (top.gg)**: [clique aqui](https://top.gg/bot/842498634822189096/vote)`)
+    .setDescription(`**Ajude a Nagatoro Music votando nele!**\n\n*Para votar clique nos links abaixo:*\n\n **Nagatoro Music (top.gg)**: [clique aqui](https://top.gg/bot/842498634822189096/vote)`)
     .setColor("#22a7cc")
     .setFooter("© Nagatoro Music - https://nagatoro.waaclive.com")
 message.channel.send(message.author, votar)
