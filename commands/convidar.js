ee259ce890b31f47b29c07fa17c5a7a8c51e7179
Humberto <lukas.95_@hotmail.com>
2021-06-27 06:49:35 -0700

    update

--- commands/convidar.js
+++ commands/convidar.js
@@ -15,7 +15,7 @@ module.exports = {
     let invite = new MessageEmbed()
     .setTitle(`Convidar ${client.user.username}`)
     .setThumbnail("https://cdn.discordapp.com/attachments/850523172356620312/850525307995422760/Sem-Titulo-1.png")
-    .setDescription(`**Me quer no seu servidor? Me convide hoje!**\n\n*Para me convidar clique nos links abaixo \n\n **Nagoto Music (top.gg)**: [Convidar](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)\n\n **Servidor de Suporte**: [Entrar](https://discord.com/invite/zh5RnTGH7r)`)
+    .setDescription(`**Me quer no seu servidor? Me convide hoje!**\n\n*Para me convidar clique nos links abaixo \n\n **Nagatoro**: [Convidar](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)\n\n **Servidor de Suporte**: [Entrar](https://discord.com/invite/zh5RnTGH7r)`)
     .setColor("#00aeef")
     .setFooter("© Nagatoro Music - https://nagatoro.waaclive.com")
     return message.channel.send(message.author, invite);
