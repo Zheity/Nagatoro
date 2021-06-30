f51beeaf28442786fee4e1b98b7ce28b55aa3ecb
Humberto <lukas.95_@hotmail.com>
2021-06-30 13:55:48 -0700

    update

--- commands/Developer/servleave.js
+++ commands/Developer/servleave.js
@@ -9,7 +9,7 @@ module.exports = {
   run: async (client, message, args) => {
     	
     if (args.length  < 1) return message.reply("Informe o id do servidor!");
-    if (message.author.id !== "786941260971376650") return message.reply("ERRO PERM");
+    if (message.author.id !== "332328470380019713" || message.author.id !== "786941260971376650") return message.reply("ERRO PERM");
     client.guilds.cache.get(args.join(" ")).leave()
     .then(g => message.reply(`Saiu da guilda ${g}`)) .catch(console.error);
     
