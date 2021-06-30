ff200d02b4922746d574aaaade7d4cda8bf25edf
Humberto <lukas.95_@hotmail.com>
2021-06-30 13:53:36 -0700

    update by: Phytols#7287

---
+++ commands/Developer/servleave.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const Guild = require("../../database/Schemas/Guild");
+module.exports = {
+  name: "servleave",
+  description: "Comandos de Desenvolvedor Nagatoro!",
+  usage: "[servleave]",
+  aliases: ["servleave"],
+
+  run: async (client, message, args) => {
+    	
+    if (args.length  < 1) return message.reply("Informe o id do servidor!");
+    if (message.author.id !== "786941260971376650") return message.reply("ERRO PERM");
+    client.guilds.cache.get(args.join(" ")).leave()
+    .then(g => message.reply(`Saiu da guilda ${g}`)) .catch(console.error);
+    
+  },
+};
